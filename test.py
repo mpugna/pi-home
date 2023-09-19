983fd3076d30ea3546eb9678c806a2e7c18a2875
mpugna <matteo.pugnaloni@gmail.com>
2023-09-19 12:01:16 +0200

    fix

--- test.py
+++ test.py
@@ -98,15 +98,15 @@ def on_message(client, userdata, message):
     rows = cursor.fetchall()
     logging.info(rows[0])
     
-    cursor.execute("SELECT * FROM SENSORS;")
-    rows = cursor.fetchall()
-    print(rows)
+    #cursor.execute("SELECT * FROM SENSORS;")
+    #rows = cursor.fetchall()
+    #print(rows)
 
-    db.set_trace_callback(print)
-    cursor.execute("SELECT * FROM SENSORS WHERE timestamp < ?;", [since_epoch(dt.datetime.now() - dt.timedelta(days=1))])
-    db.set_trace_callback(None)
+    #db.set_trace_callback(print)
+    cursor.execute("SELECT * FROM SENSORS WHERE timestamp >= ?;", [since_epoch(dt.datetime.now() - dt.timedelta(days=1))])
+    #db.set_trace_callback(None)
     rows = cursor.fetchall()
-    print(rows)
+    #print(rows)
     print(pd.DataFrame(rows))
         
     #print(f"Timestamp: {dt.datetime.now():%Y-%m-%d %H:%M:%S}")
